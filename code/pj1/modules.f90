605c0ba4393be84280dbdb3a52af72501f40f203
ldhalstrom <loganhalstrom@gmail.com>
2015-10-12 14:21:04 -0700

    finished modules

--- code/pj1/modules.f90
+++ code/pj1/modules.f90
@@ -33,8 +33,7 @@ CONTAINS
         IMAX = N
         JMAX = N
     END SUBROUTINE GRIDSIZE
-
-END MODULE
+END MODULE CONSTANTS
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!! WALL CLOCK TIME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -59,7 +58,7 @@ CONTAINS
         wall_time = DFLOAT(clock_end - clock_start) / DFLOAT(clock_rate)
         PRINT*, 'Solver wall clock time (seconds):', wall_time
     END SUBROUTINE end_clock
-END MODULE
+END MODULE CLOCK
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!! INITIALIZE GRID !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -77,12 +76,11 @@ MODULE MAKEGRID
         INTEGER :: i, j
         ! Grid points, see cooridinate rotaion equations in problem statement
         REAL(KIND=8) :: xp, yp, x, y
-        ! Temperature at each point
-        REAL(KIND=8) :: T
+        ! Temperature at each point, temporary variable to hold temperature sum
+        REAL(KIND=8) :: T, Ttmp
         ! Iteration Parameters: timestep, secondary cell volume,
                                     ! equation constant term
         REAL(KIND=8) :: dt, V2nd, term
-
     END TYPE GRID
 
 CONTAINS
@@ -90,35 +88,35 @@ CONTAINS
         ! Mesh points (derived data type)
         TYPE(GRID), TARGET :: mesh(1:IMAX, 1:JMAX)
         ! Pointer for mesh points
-        TYPE(GRID), POINTER :: p
+        TYPE(GRID), POINTER :: m
         INTEGER :: i, j
 
         DO J = 1, JMAX
             DO I = 1, IMAX
-                p => mesh(i, j)
+                m => mesh(i, j)
                 ! 'p' points to 'mesh', i is variable in derived data type
                     ! accessed by '%'
                 ! MAKE SQUARE GRID
-                p%i = i
-                p%j = j
+                m%i = i
+                m%j = j
                 ! ROTATE GRID
-                p%xp = COS( 0.5D0 * pi * DFLOAT(IMAX - i) / DFLOAT(IMAX - 1) )
-                p%yp = COS( 0.5D0 * pi * DFLOAT(JMAX - j) / DFLOAT(JMAX - 1) )
+                m%xp = COS( 0.5D0 * pi * DFLOAT(IMAX - i) / DFLOAT(IMAX - 1) )
+                m%yp = COS( 0.5D0 * pi * DFLOAT(JMAX - j) / DFLOAT(JMAX - 1) )
 
-                p%x = p%xp * COS(rot) + (1.D0 - p%yp ) * SIN(rot)
-                p%y = p%yp * COS(rot) + (p%xp) * SIN(rot)
+                m%x = p%xp * COS(rot) + (1.D0 - m%yp ) * SIN(rot)
+                m%y = p%yp * COS(rot) + (m%xp) * SIN(rot)
             END DO
         END DO
     END SUBROUTINE init_grid
 
     SUBROUTINE init_temp(p, T)
         ! Initialize temperature across mesh
-        ! p --> pointer for mesh vector
+        ! m --> pointer for mesh vector
         ! T --> initial temperature profile
-        TYPE(GRID), INTENT(INOUT) :: p
+        TYPE(GRID), INTENT(INOUT) :: m
         REAL(KIND=8) :: T
         ! SET MESH POINTS WITH INITIAL TEMPERATURE PROFILE
-        p%T = T
+        m%T = T
     END SUBROUTINE init_temp
 END MODULE MAKEGRID
 
@@ -225,5 +223,63 @@ CONTAINS
         END DO
     END SUBROUTINE calc_constants
 END MODULE CELLS
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!! CALCULATE TEMPERATURE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+MODULE TEMPERATURE
+    ! Calculate and store new temperature distribution for given iteration
+    USE MAKEGRID
+    USE CELLS
+
+    IMPLICIT NONE
+    PUBLIC
+
+CONTAINS
+    SUBROUTINE derivatives(m, c)
+        ! Calculate first and second derivatives for finite-volume scheme
+        TYPE(GRID), INTENT(INOUT) :: m(1:IMAX, 1:JMAX)
+        TYPE(MESH), INTENT(INOUT) :: m(1:IMAX-1, 1:JMAX-1)
+        ! Areas for first derivatives
+        REAL(KIND=8) :: Ayi, Axi, Ayj, Axj
+        ! First partial derivatives of temperature in x and y directions
+        REAL(KIND=8) :: dTdx, dTdy
+        INTEGER :: i, j
+
+        ! CALC CELL AREAS
+        Axi(i,j) = m(i,j+1)%x - m(i,j)%x
+        Axj(i,j) = m(i+1,j)%x - m(i,j)%x
+        Ayi(i,j) = m(i,j+1)%y - m(i,j)%y
+        Ayj(i,j) = m(i+1,j)%y - m(i,j)%y
+
+        ! RESET SUMMATION
+        m%Ttmp = 0.D0
+
+        DO j = 1, JMAX - 1
+            DO i = 1, IMAX - 1
+                ! CALC FIRST DERIVATIVES
+                dTdx = + 0.5d0 &
+                            * (( m(i+1,j)%T + m(i+1,j+1)%T ) * Ayi(i+1,j) &
+                            -  ( m(i,  j)%T + m(i,  j+1)%T ) * Ayi(i,  j) &
+                            -  ( m(i,j+1)%T + m(i+1,j+1)%T ) * Ayj(i,j+1) &
+                            +  ( m(i,  j)%T + m(i+1,  j)%T ) * Ayj(i,  j) &
+                                ) / c(i, j)%V
+                dTdy = - 0.5d0 &
+                            * (( m(i+1,j)%T + m(i+1,j+1)%T ) * Axi(i+1,j) &
+                            -  ( m(i,  j)%T + m(i,  j+1)%T ) * Axi(i,  j) &
+                            -  ( m(i,j+1)%T + m(i+1,j+1)%T ) * Axj(i,j+1) &
+                            +  ( m(i,  j)%T + m(i+1,  j)%T ) * Axj(i,  j) &
+                                ) / c(i, j)%V
+
+                ! Alternate distributive scheme second-derivative operator.
+                m(i+1,  j)%Ttmp = m(i+1,  j)%Ttmp + p(i+1,  j)%term * ( c(i, j)%yNN * dTdx + c(i, j)%xPP * dTdy )
+                m(i,    j)%Ttmp = m(i,    j)%Ttmp + p(i,    j)%term * ( c(i, j)%yPN * dTdx + c(i, j)%xNP * dTdy )
+                m(i,  j+1)%Ttmp = m(i,  j+1)%Ttmp + p(i,  j+1)%term * ( c(i, j)%yPP * dTdx + c(i, j)%xNN * dTdy )
+                m(i+1,j+1)%Ttmp = m(i+1,j+1)%Ttmp + p(i+1,j+1)%term * ( c(i, j)%yNP * dTdx + c(i, j)%xPN * dTdy )
+            END DO
+        END DO
+    END SUBROUTINE derivatives
+END MODULE TEMPERATURE
 
 
