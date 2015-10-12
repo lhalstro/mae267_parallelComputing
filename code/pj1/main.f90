7a3180e5340b0c6fd731c81705936197aa26c5f5
ldhalstrom <loganhalstrom@gmail.com>
2015-10-12 10:25:11 -0700

    started project 1

---
+++ code/pj1/main.f90
@@ -0,0 +1,20 @@
+program heatTrans
+
+! MAE 267
+! PROJECT 1
+! LOGAN HALSTROM
+! 12 OCTOBER 2015
+
+! DESCRIPTION:  Solve heat conduction equation for single block of steel.
+
+!   DECLARE VARIABLES
+    implicit none
+
+!   MAKE GRID
+
+!   SOLVE HEAT CONDUCTION
+
+
+
+
+end program heatTrans
---
+++ code/pj1/subroutines.f90
@@ -0,0 +1,16 @@
+program subroutines
+
+! MAE 267
+! PROJECT 1
+! LOGAN HALSTROM
+! 12 OCTOBER 2015
+
+! DESCRIPTION:  Subroutines used for solving heat conduction of steel plate.
+
+!   DECLARE VARIABLES
+    implicit none
+
+
+
+
+end program subroutines
